8071b9e140537e0abdbe0e466a7b41780ce18cc5
Adrian W <awidlak@adrianwii.pl>
2025-12-20 14:24:03 +0100

    Create movie with mongoo

--- src/controllers/movie.controllers.js
+++ src/controllers/movie.controllers.js
@@ -1,41 +1,43 @@
-export function getMovies(req, res) {
+import { Movie } from "../models/Movie.js";
 
-    const movies = [
-        { title: "Inception", year: 2010},
-        { title: "Interstellar", year: 2014},
-        { title: "The Matrix", year: 1999}
-    ];
-
-
+export const getMovies = async (req, res) => {
+  try {
+    const movies = await Movie.find();
     res.status(200).json(movies);
-}
-
+  } catch (e) {
+    res.status(404).json({ message: `Not found ${e.message}` });
+  }
+};
 
 export function getMovie(req, res) {
-    const id = req.params.id;
-    // find movie in database
+  const id = req.params.id;
+  // find movie in database
 
-    if(id === 0) {
-        res.status(404).json({message: "movie not found"})
-    }
+  if (id === 0) {
+    res.status(404).json({ message: "movie not found" });
+  }
 
-    res.json({
-        id: 5,
-        title: "avatar",
-        description: "",
-        year: "2021",
-        director: "James Cameron",
-        likes: 200
-    })
-
+  res.json({
+    id: 5,
+    title: "avatar",
+    description: "",
+    year: "2021",
+    director: "James Cameron",
+    likes: 200,
+  });
 }
 
-export function createMovie(req, res) {
-    const newMovie = req.body;
-    // wstaw newMovie do bazy danych
-    console.log(
-        `🎬 [MOVIE CREATED] ${newMovie.title}`, 
-        newMovie
-    )
-    res.status(201).json(newMovie);
-}
+export const createMovie = async (req, res) => {
+  const { title, description, year, director } = req.body;
+
+  const movie = await Movie.create({
+    title,
+    description,
+    year,
+    director,
+  });
+
+  // wstaw newMovie do bazy danych
+  console.log(`🎬 [MOVIE CREATED]`);
+  res.status(201).json(movie);
+};
