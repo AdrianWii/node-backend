c42580e3402a2a1773d96f2b884fce2b802fb476
Adrian Widlak <awidlak@adrianwii.pl>
2025-12-20 09:01:09 +0100

    Update project structure

--- src/controllers/movie.controllers.js
+++ src/controllers/movie.controllers.js
@@ -1,5 +1,12 @@
 
 export function getMovies(req, res) {
 
-    res.status(200).json({ok: "lista filmów"});
+    const movies = [
+        { title: "Inception", year: 2010},
+        { title: "Interstellar", year: 2014},
+        { title: "The Matrix", year: 1999}
+    ];
+
+
+    res.status(200).json(movies);
 }
--- src/server.js
+++ src/server.js
@@ -5,6 +5,15 @@ import movieRoute from './routes/movies.routes.js';
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3001");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  if (req.method === "OPTIONS") return res.sendStatus(204);
+  next();
+});
+
+
 app.use(express.json()); //middleware
 app.use(serverLogger);
 
